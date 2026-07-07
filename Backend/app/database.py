b1d2196304d70e52ed4c62498e9c6aadfb4db665
PRIYANSHU RAJ <priyanshuraj2011005@gmail.com>
2026-07-07 22:07:04 +0530

    Refactor database.py with comments and cleanup
    Added comments for clarity and removed unused code.

--- Backend/app/database.py
+++ Backend/app/database.py
@@ -1,27 +1,20 @@
-import logging
-from pymongo.errors import ConnectionFailure, ServerSelectionTimeoutError
-from motor.motor_asyncio import AsyncIOMotorClient
+import logging 
+from pymongo.errors import ConnectionFailure, ServerSelectionTimeoutError    #used forr printing or storing the error of info msg...
+from motor.motor_asyncio import AsyncIOMotorClient   #motor mongo db ka async driver hain
 from app.core.config import (
     MONGODB_URI,
     DATABASE_NAME,
     USERS_COLLECTION,
 )
-# from app.core.config import TRANSLATIONS_COLLECTION
-
-
-
-# Initialize logger
 logger = logging.getLogger(__name__)
 
-# Create the client instance (does not open connection yet)
+
 client = AsyncIOMotorClient(MONGODB_URI, serverSelectionTimeoutMS=5000)
 
 db = client[DATABASE_NAME]
 
 users_collection = db[USERS_COLLECTION]
-# translations_collection = db[TRANSLATIONS_COLLECTION]
 
-# Call this function inside your framework's startup event (e.g., FastAPI lifespan)
 async def check_database_connection():
     try:
         # Force a connection attempt by pinging the admin database
@@ -29,7 +22,7 @@ async def check_database_connection():
         logger.info("Successfully connected to MongoDB Atlas!")
     except (ConnectionFailure, ServerSelectionTimeoutError) as e:
         logger.critical(f"Database connection failed: {e}")
-        # Insert your fallback logic here (e.g., sys.exit(1) to stop the server)
+        # Insert your fallback logic here 
         raise e
 
 users_collection = db[USERS_COLLECTION]
